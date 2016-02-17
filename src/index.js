20c2a1206eed5e33be47766969ad97b9643b35d1
Jesse Lamb <jesse.lamb@builddirect.com>
2016-02-16 16:59:04 -0800

    inital commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+/* xhr Upload function that posts data to the given url
+
+	url: String value representing where to post the data
+
+	data: FormData value that is to be sent eg:
+	      const data = new FormData();
+	      data.append('file', file);
+	      data.append('formValue', 'value');
+
+	updatePercentageFn: is called each time there is a progress event fired on the
+	                    input. It is passed a value between 0-100 representing the
+	                    % done
+
+	onCompleteFn: is called once the upload is complete and is passed the 
+	              JSON.parse()'d' responseText as a parameter
+*/
+export function xhrUpload(url, data, updatePercentageFn, onCompleteFn) {
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.upload.addEventListener('progress', (event) => {
+		if (event.lengthComputable) {
+			updatePercentageFn( Math.round((event.loaded * 100) / event.total) );
+		}
+	}, false);
+
+	xhr.addEventListener('load', (/*event*/) => {
+		updatePercentageFn( 100 );
+		onCompleteFn( JSON.parse(xhr.responseText) );
+	}, false);
+
+	xhr.open('POST', url, true);
+	xhr.send(data);
+}
